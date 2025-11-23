b5d3272c7054e056a143e4debf3407158b81b04e
hijazi77 <muhammed7712@outlook.com>
2025-11-23 21:08:16 +0200

    feat: configure VITE_BASE_PATH in vite.config.js and add nixpacks.toml for deployment setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Blacktape-Website/",
+  base: process.env.VITE_BASE_PATH || "/Blacktape-Website/",
   plugins: [tailwindcss(), react()],
 });
